d0dea35df70cc9494f3944f416279fd1beef31dc
kuldeepak05 <kuldeepak05@gmail.com>
2025-10-17 09:49:25 +0530

    Initial commit

---
+++ app/routes/app.jsx
@@ -0,0 +1,36 @@
+import { Link, Outlet, useLoaderData, useRouteError } from "react-router";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return {
+    // eslint-disable-next-line no-undef
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  };
+};
+
+export default function App() {
+  const { apiKey } = useLoaderData();
+
+  return (
+    <AppProvider embedded apiKey={apiKey}>
+      <ui-nav-menu>
+        <Link to="/app" rel="home">Home</Link>
+        <Link to="/products">All Products</Link>
+        <Link to="/app/additional">Additional Page</Link>
+      </ui-nav-menu>
+      <Outlet />
+    </AppProvider>
+  );
+}
+
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
+
+export const headers = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
